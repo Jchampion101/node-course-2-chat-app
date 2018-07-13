20cc6d3a8ada419bb813685fcded38a478cc25fe
EkeadaChristian <crix4luv@gmail.com>
2018-07-13 13:50:33 +0100

    Format timestamps using momentjs

--- public/js/index.js
+++ public/js/index.js
@@ -9,18 +9,19 @@
             });
 
             socket.on('newMessage', function (message) {
-                console.log('newMessage', message);
+                var formattedTime = moment(message.createdAt).format('h:mm a');
                 var li = jQuery('<li></li>');
-                li.text(`${message.from}: ${message.text}`);
+                li.text(`${message.from} ${formattedTime}: ${message.text}`);
 
                 jQuery('#messages').append(li);
             });
 
 socket.on('newLocationMessage', function (message) {
+    var formattedTime = moment(message.createdAt).format('h:mm a');
     var li = jQuery('<li></li>');
     var a = jQuery('<a target="_blank">My current location</a>');
 
-    li.text(`${message.from}: `);
+    li.text(`${message.from} ${formattedTime}: `);
     a.attr('href', message.url);
     li.append(a);
     jQuery('#messages').append(li);
@@ -39,7 +40,7 @@ socket.on('newLocationMessage', function (message) {
                 });
             });
 
-            var locationButton = jquery('#send-location');
+            var locationButton = jQuery('#send-location');
             locationButton.on('click', function () {
                 if (!navigator.geolocation) {
                     return alert('Goelocation not supported by your browser.');
